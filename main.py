a9fb53901803c9aba682cf90f69585992eb4c623
hnpark <hnpark@4dreplay.com>
2023-03-10 18:39:45 +0900

    -video 코드 추가, video import문 수정, json model에 camIdx 추가, 멀티스레딩 테스트 중

--- main.py
+++ main.py
@@ -7,40 +7,59 @@ from flask_restx import fields, Resource, Api, reqparse, marshal
 # from exodus import *
 # from job_manager import JobManager
 # from db_layer import NewPool, DBLayer
-from video import video as v
+from video import video
+import threading
 
 app = Flask(__name__)
 api = Api(app, version='0.1', title='KAIROS')
 app.config.SWAGGER_UI_DOC_EXPANSION = 'full'
 
-
+##
 conn_video = api.model('conn_vide', {
-    'type': fields.String,  
-    'address': fields.String
+    'type': fields.String,
+    'address': fields.String,
+    'camIdx': fields.String
 })
 
 
 @api.route('/kairos/conn_video')
 @api.doc()
 class ConnVideo(Resource):
-	@api.expect(conn_video)
-	def post(self, model=conn_video):
+    @api.expect(conn_video)
+    def post(self, model=conn_video):
 
-		parser = reqparse.RequestParser()
-		parser.add_argument('type', type=str)
-		parser.add_argument('address', type=str)
-		args = parser.parse_args()
-		print(args)
-        
-		v.Video(args)
+        parser = reqparse.RequestParser()
+        parser.add_argument('type', type=str)
+        parser.add_argument('address', type=str)
+        parser.add_argument('camIdx', type=str)
+        args = parser.parse_args()
+        print(args)
 
-		result = {
-		'status': 0,
-		'job_id': 0,
-		'message': 'SUCCESS',
-		}
+        vid = video.Video(args)
+        # vid.play()
 
-		return result
+        # test multi thread
+        video_list = []
+        video_list.append(args['address'])
+        video_list.append(args['address'])
+        print(video_list)
+
+        # players = [video.Video(vi) for vi in video_list]
+        # threads = [threading.Thread(target=player.play) for player in players]
+
+        # for thread in threads:
+        #     thread.start()
+        # for thread in threads:
+        #     thread.join()
+
+        result = {
+            'status': 0,
+            'job_id': 0,
+            'message': 'SUCCESS',
+        }
+
+        return result
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=3060)
--- video/video.py
+++ video/video.py
@@ -1,9 +1,50 @@
 import os
+import cv2
+# import threading
 
 
-class Video() :
-    
-	def __init__(self, args) :
-		print("video class is created")
-		print(args)
+class Video():
 
+    def __init__(self, args):
+        self.type = args['type']
+        self.path = args['address']
+        self.camIdx = args['camIdx']
+        print("video class is created")
+        # print(args)
+        print("type : ", self.type)
+        print("path : ", self.path)
+        print("cam index : ", self.camIdx)
+
+        try:
+            self.cap = cv2.VideoCapture(self.path)
+            if not self.cap.isOpened():
+                raise IOError("Could not open video file.")
+
+        except cv2.error as e:
+            print(f"OpenCV exception: {e}")
+
+        except IOError as e:
+            print(f"Error: {e}")
+
+        except Exception as e:
+            print(f"Error: {e}")
+        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
+
+    def play(self):
+
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                print('do not read video')
+                break
+
+            # tracking
+
+            frame = cv2.resize(frame, (1920, 1080), cv2.INTER_AREA)
+            cv2.imshow(self.path, frame)
+
+            if cv2.waitKey(int(1000/self.fps)) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
